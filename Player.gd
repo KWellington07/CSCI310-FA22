f08bb1a81d6391945fd4ccd2003ed957eee9e887
KWellington07 <70113137+KWellington07@users.noreply.github.com>
2022-11-02 00:04:53 -0500

    Update Player.gd

--- Player.gd
+++ Player.gd
@@ -1,4 +1,7 @@
 extends KinematicBody2D
+class_name Player
+
+enum { MOVE, CLIMB }
 
 export(int) var JUMP_FORCE = -130
 export(int) var JUMP_RELEASE_FORCE = -70
@@ -9,26 +12,26 @@ export(int) var GRAVITY = 4
 export(int) var ADDITIONAL_FALL_GRAVITY = 4
 
 var velocity = Vector2.ZERO
-var motion = Vector2()
+var state = MOVE
 
 onready var animatedSprite = $AnimatedSprite
-var attacking = false
-var attack_anim = null
-var anim_numb = 1
-
 onready var ladderCheck = $LadderCheck
-
 
 func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
-	apply_gravity() 
-	attack_anim = "Knuckle"+str(anim_numb)
 	var input = Vector2.ZERO
-
-	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-
+	input.x = Input.get_axis("ui_left", "ui_right")
+	input.y = Input.get_axis("ui_up", "ui_down")
+	match state:
+		MOVE: move_state(input)
+		CLIMB: climb_state(input)
+	
+func move_state(input):
+	if is_on_ladder() and Input.is_action_pressed("ui_up"): state = CLIMB
+	
+	apply_gravity()
 	if input.x == 0:
 		apply_friction()
 		animatedSprite.animation = "Idle"
@@ -54,22 +57,28 @@ func _physics_process(delta):
 			animatedSprite.animation = "Falling"
 			velocity.y += ADDITIONAL_FALL_GRAVITY
 	
-	if Input.is_action_just_pressed("light_attack"):
-		$Timer.start()
-		attack()
-		
-		if $Timer.time_left > 0:
-			anim_numb += 1
-		if anim_numb == 4:
-			anim_numb = 1
-			
-	
 	var was_in_air = not is_on_floor()	
 	velocity = move_and_slide(velocity, Vector2.UP)
 	var just_landed = is_on_floor() and was_in_air
 	if just_landed:
 		animatedSprite.animation = "Landing"
+	
+func climb_state(input):
+	if not is_on_ladder(): state = MOVE
+	#if input.length() != 0:
+	#	animatedSprite.animation = "Climb"
+	#else:
+	#	animatedSprite.stop()
+		
+	velocity = input * 50
+	velocity = move_and_slide(velocity, Vector2.UP)
+	animatedSprite.animation = "Climb"
 
+func is_on_ladder():
+	if not ladderCheck.is_colliding(): return false
+	var collider = ladderCheck.get_collider()
+	if not collider is Ladder: return false
+	return true
 
 func apply_gravity():
 	velocity.y += GRAVITY
@@ -80,15 +89,3 @@ func apply_friction():
 
 func apply_acceleration(amount):
 	velocity.x = move_toward(velocity.x, MAX_SPEED * amount, ACCELERATION)
-	
-func attack():
-	attacking = true
-	$AnimatedSprite.play(attack_anim)
-	yield($AnimatedSprite, "animation_finished")
-	attacking = false
-	
-func _on_Timer_timeout():
-	anim_numb = 1
-
-
-	
